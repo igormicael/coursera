9cdbf05fa122c5eb38fb6e3312e31af827e8e464
igor micael <igor.m.peixoto@gmail.com>
2015-12-19 19:31:16 -0200

    angular form complete

--- angularJS/week2/conFusion/app/scripts/app.js
+++ angularJS/week2/conFusion/app/scripts/app.js
@@ -13,34 +13,34 @@ angular.module('confusionApp', [])
                     label:'Hot',
                     price:'4.99',
                     description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-                     comment: ''
+                    comment: ''
                   },
                   {
-                     name:'Zucchipakoda',
-                     image: 'images/zucchipakoda.png',
-                     category: 'appetizer',
-                     label:'',
-                     price:'1.99',
+                    name:'Zucchipakoda',
+                    image: 'images/zucchipakoda.png',
+                    category: 'appetizer',
+                    label:'',
+                    price:'1.99',
                     description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-                     comment: ''
+                    comment: ''
                   },
                   {
-                     name:'Vadonut',
-                     image: 'images/vadonut.png',
-                     category: 'appetizer',
-                     label:'New',
-                     price:'1.99',
+                    name:'Vadonut',
+                    image: 'images/vadonut.png',
+                    category: 'appetizer',
+                    label:'New',
+                    price:'1.99',
                     description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
-                     comment: ''
+                    comment: ''
                   },
                   {
-                     name:'ElaiCheese Cake',
-                     image: 'images/elaicheesecake.png',
+                    name:'ElaiCheese Cake',
+                    image: 'images/elaicheesecake.png',
                     category: 'dessert',
-                     label:'',
+                    label:'',
                     price:'2.99',
                     description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-                     comment: ''
+                    comment: ''
                   }
                   ]; 
 
@@ -70,16 +70,28 @@ angular.module('confusionApp', [])
   }])
 
 .controller('ContactController', ['$scope', function($scope) {
+    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+    var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
 
-    $scope.feedback = {mychannel:"", 
-                       firstName:"", 
-                       lastName:"",
-                       agree:false, 
-                       email:"" };
-}])
+    $scope.channels = channels;
+    $scope.invalidChannelSelection = false;
+                        }])
 
 .controller('FeedbackController', ['$scope', function($scope) {
+      $scope.sendFeedback = function() {
+        console.log($scope.feedback);
+        if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+          $scope.invalidChannelSelection = true;
+          console.log('incorrect');
+        }else {
+            $scope.invalidChannelSelection = false;
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+            $scope.feedback.mychannel="";
 
+            $scope.feedbackForm.$setPristine();
+            console.log($scope.feedback);
+        }
+    };
 }])
 
 ;
